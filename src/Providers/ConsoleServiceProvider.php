390c169285e3a51bfcf350773c1a125bfd4bbf1e
wanghouting <wanghouting@163.com>
2019-01-14 02:43:49 +0800

    修改provide

--- src/Providers/ConsoleServiceProvider.php
+++ src/Providers/ConsoleServiceProvider.php
@@ -1,9 +1,9 @@
 <?php
 
-namespace Hazel\Modules\Providers;
+namespace HazelFiver\Modules\Providers;
 
 use Illuminate\Support\ServiceProvider;
-
+use HazelFiver\Modules\Commands\ModuleMakeCommand;
 
 class ConsoleServiceProvider extends \Nwidart\Modules\Providers\ConsoleServiceProvider
 {
